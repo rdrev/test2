69962756c461d947d6bbfd19730ab0ba0e604217
Никита Шихов <rdrev2001@gmail.com>
2020-11-05 22:59:39 +0300

    Navigation

--- testWord/TablePage.xaml.cs
+++ testWord/TablePage.xaml.cs
@@ -23,8 +23,42 @@ namespace testWord
         public TablePage(bool b)
         {
             InitializeComponent();
+
+            var comboList = testWordEntities1.GetTestWordEntities1().Бригады.ToList();
+
+            comboList.Insert(0, new Бригады
+            {
+                Машина = "Все"
+            });
+            CBB.ItemsSource = comboList;
+
+            CBB.SelectedIndex = 0;
+
             LVC.ItemsSource = testWordEntities1.GetTestWordEntities1().Сотрудники.ToList();
             DGT.ItemsSource = testWordEntities1.GetTestWordEntities1().Задачи.ToList();
+
+            update();
+        }
+
+        private void update()
+        {
+            var comboList = testWordEntities1.GetTestWordEntities1().Сотрудники.ToList();
+            if (CBB.SelectedIndex > 0)
+                comboList = comboList.Where(p => p.Бригады == (CBB.SelectedItem as Бригады)).ToList();
+
+            comboList = comboList.Where(p => p.Имя.ToLower().Contains(TBN.Text.ToLower())).ToList();
+
+            LVC.ItemsSource = comboList;
+        }
+
+        private void CBB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            update();
+        }
+
+        private void TBN_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            update();
         }
     }
 }
