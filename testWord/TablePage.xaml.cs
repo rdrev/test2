2c4929a58a161d33c6395e7e12a5e25e8484ba05
Никита Шихов <rdrev2001@gmail.com>
2020-11-06 13:00:25 +0300

    Navigation2

--- testWord/TablePage.xaml.cs
+++ testWord/TablePage.xaml.cs
@@ -30,35 +30,51 @@ namespace testWord
             {
                 Машина = "Все"
             });
-            CBB.ItemsSource = comboList;
+            CBB1.ItemsSource = comboList;
+            CBB2.ItemsSource = comboList;
 
-            CBB.SelectedIndex = 0;
+            CBB1.SelectedIndex = 0;
+            CBB2.SelectedIndex = 0;
 
             LVC.ItemsSource = testWordEntities1.GetTestWordEntities1().Сотрудники.ToList();
             DGT.ItemsSource = testWordEntities1.GetTestWordEntities1().Задачи.ToList();
 
-            update();
+            update1();
+            update2();
         }
 
-        private void update()
+        private void update2()
         {
             var comboList = testWordEntities1.GetTestWordEntities1().Сотрудники.ToList();
-            if (CBB.SelectedIndex > 0)
-                comboList = comboList.Where(p => p.Бригады == (CBB.SelectedItem as Бригады)).ToList();
+            if (CBB2.SelectedIndex > 0)
+                comboList = comboList.Where(p => p.Бригады == (CBB1.SelectedItem as Бригады)).ToList();
 
             comboList = comboList.Where(p => p.Имя.ToLower().Contains(TBN.Text.ToLower())).ToList();
 
             LVC.ItemsSource = comboList;
         }
+        private void update1()
+        {
+            var comboList = testWordEntities1.GetTestWordEntities1().Задачи.ToList();
+            if (CBB1.SelectedIndex > 0)
+                comboList = comboList.Where(p => p.Бригады == (CBB1.SelectedItem as Бригады)).ToList();
 
+
+            DGT.ItemsSource = comboList;
+        }
         private void CBB_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            update();
+            update2();
         }
 
         private void TBN_TextChanged(object sender, TextChangedEventArgs e)
         {
-            update();
+            update2();
+        }
+
+        private void CBB1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            update1();
         }
     }
 }
